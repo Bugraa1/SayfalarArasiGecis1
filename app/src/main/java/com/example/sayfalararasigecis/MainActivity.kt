634bd15cfcddc7c1ce366ce666f27084d54e99f8
Bugraa1 <yusufbugrayildirim790@gmail.com>
2024-10-19 17:09:09 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/sayfalararasigecis/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.sayfalararasigecis
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.sayfalararasigecis.ui.theme.SayfalarArasiGecisTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SayfalarArasiGecisTheme {
+                SayfaGecis()
+
+            }
+        }
+    }
+}
+@Composable
+fun SayfaGecis(){
+    val navController = rememberNavController()
+
+    NavHost(navController=navController, startDestination = "ilk_sayfa"){
+
+        composable(route = "ilk_sayfa") {
+            BirinciSayfa(navController)
+        }
+        composable(route = "ikinci_sayfa") {
+            IkinciSayfa(navController)
+        }
+        composable(route = "ucuncu_sayfa") {
+            UcuncuSayfa(navController)
+        }
+
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SayfalarArasiGecisTheme {
+    }
+}
